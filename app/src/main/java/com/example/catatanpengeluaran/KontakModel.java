3abce88ab11d3620bc423331f4528f27c448aada
Ariflutfi <arif.kurniawan@students.amikom.ac.id>
2023-06-08 10:20:33 +0700

    8 juni 2023

---
+++ app/src/main/java/com/example/catatanpengeluaran/KontakModel.java
@@ -0,0 +1,49 @@
+package com.example.catatanpengeluaran;
+
+import java.io.Serializable;
+
+public class KontakModel implements Serializable {
+    public String nama;
+    public String hp;
+    public String status;
+    public String color;
+
+    public KontakModel(String nama, String hp, String status, String color) {
+        this.nama = nama;
+        this.hp = hp;
+        this.status = status;
+        this.color = color;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getHp() {
+        return hp;
+    }
+
+    public void setHp(String hp) {
+        this.hp = hp;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
